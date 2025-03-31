ec813a75fb3b9e4ce974cdb2ab70d48594e4d58a
sabatek70 <137089474+sabatek70@users.noreply.github.com>
2025-03-31 03:50:16 +0200

    Create OddEven.c

---
+++ OddEven.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    for(int limit, start, step, code = scanf("%d%d%d", &start, &limit, &step, printf("<start> <end> <step>\n" ));start<=limit; printf("\n", (start%2==0)?printf(" : even", start+=step):printf(" : odd", start+=step))) printf("%d", start);
+    return 0;
+}
+
+// goes from start to end by step and prints whether the numbers are even or odd
