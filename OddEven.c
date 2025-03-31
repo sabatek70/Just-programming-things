c1959414390ba40323732b1554765ad9728fcf8a
sabatek70 <137089474+sabatek70@users.noreply.github.com>
2025-03-31 04:30:21 +0200

    Fixed out of bounds
    No breaking!

--- OddEven.c
+++ OddEven.c
@@ -1,12 +1,12 @@
-
 #include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-    for(int input_start, input_limit, step, code = scanf("%d%d%d", &input_start, &input_limit, &step, printf("<start> <end> <step>\n" )), start = input_start, limit = input_limit;(input_start<=input_limit)?(start<=limit):(limit<=start); printf("\n", (start%2==0)?printf(" : even", start+=step):printf(" : odd", start+=step))) printf("%d", start);
+    for(int input_start, input_limit, step, code = scanf("%d%d%d", &input_start, &input_limit, &step, printf("<start> <end> <step>\n")), start = input_start, limit = input_limit, a = printf("", (input_limit-input_start>0&&step<=0)?start=limit+1:0, (input_limit-input_start<0&&step>=0)?limit=start+1:0);(input_start<=input_limit)?(start<=limit):(limit<=start); printf("\n", (start%2==0)?printf(" : even", start+=step):printf(" : odd", start+=step))) printf("%d", start);
     return 0;
 }
 
 // goes from start to end by step and prints whether the numbers are even or odd
 // all only using a bodyless for
 //yes it does go backwards, do negative step
+//yes it will stop instead of running towards either one of the infinities if you try to set the step to be directed outwards the scope
