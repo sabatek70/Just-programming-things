52fb3ddd16623a7aa6588e3fd3e0ceb3ed515fd5
sabatek70 <137089474+sabatek70@users.noreply.github.com>
2025-03-31 04:52:09 +0200

    Now 100% bodyless
    I found that I have put a print after the for, so it DID in fact have a body. But now it really doesn't though

--- OddEven.c
+++ OddEven.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-    for(int input_start, input_limit, step, code = scanf("%d%d%d", &input_start, &input_limit, &step, printf("<start> <end> <step>\n")), start = input_start, limit = input_limit, a = printf("", (input_limit-input_start>0&&step<=0)?start=limit+1:0, (input_limit-input_start<0&&step>=0)?limit=start+1:0);(input_start<=input_limit)?(start<=limit):(limit<=start); printf("\n", (start%2==0)?printf(" : even", start+=step):printf(" : odd", start+=step))) printf("%d", start);
+    for(int input_start, input_limit, step, last_start, code = scanf("%d%d%d", &input_start, &input_limit, &step, printf("<start> <end> <step>\n")), start = input_start, limit = input_limit, a = printf("", (input_limit-input_start>0&&step<=0)?(start=limit+1):0, (input_limit-input_start<0&&step>=0)?(limit=start+1):0);(input_start<=input_limit)?(last_start = start, start<=limit):(last_start = start, limit<=start); printf("\n", (last_start%2==0)?printf("%d : even", last_start, start+=step):printf("%d : odd", last_start, start+=step)));
     return 0;
 }
 
