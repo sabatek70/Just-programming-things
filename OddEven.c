54d9b1a9703ebfbb053f0a0d4fdeb7e39ea84e0f
sabatek70 <137089474+sabatek70@users.noreply.github.com>
2025-03-31 04:06:38 +0200

    now goes backwards too

--- OddEven.c
+++ OddEven.c
@@ -1,11 +1,12 @@
+
 #include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-    for(int limit, start, step, code = scanf("%d%d%d", &start, &limit, &step, printf("<start> <end> <step>\n" ));start<=limit; printf("\n", (start%2==0)?printf(" : even", start+=step):printf(" : odd", start+=step))) printf("%d", start);
+    for(int input_start, input_limit, step, code = scanf("%d%d%d", &input_start, &input_limit, &step, printf("<start> <end> <step>\n" )), start = input_start, limit = input_limit;(input_start<=input_limit)?(start<=limit):(limit<=start); printf("\n", (start%2==0)?printf(" : even", start+=step):printf(" : odd", start+=step))) printf("%d", start);
     return 0;
 }
 
 // goes from start to end by step and prints whether the numbers are even or odd
 // all only using a bodyless for
-// no it doesn't go backwards I didn't really want to think about that, I probably could, but I won't, or maybe, idk
+//yes it does go backwards, do negative step
