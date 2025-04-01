ffa21039784ee1a1b7eb771dddef96f8bd9c145c
sabatek70 <137089474+sabatek70@users.noreply.github.com>
2025-04-02 00:56:26 +0200

    Clarity update
    Now it should be much more clear and less unnecessarily convoluted

--- OddEven.c
+++ OddEven.c
@@ -12,20 +12,18 @@ int main(int argc, char* argv[])
         boundsTop = (input_limit - input_start < 0 && step >= 0) ? (limit = start + 1 ) : 0;
     
         (input_start <= input_limit)
-        ? (last_start = start, start<=limit)
-        : (last_start = start, limit<=start);
-    
-        printf
-        (   "\n%.i",
-            (last_start%2==0)
-            ? 0*printf("%d%.i: even", last_start, 0*(start+=step))
-            : 0*printf("%d%.i: odd", last_start, 0*(start+=step))
-        )
+        ? (start<=limit) //going up
+        : (limit<=start); //going down
+        
+        last_start = start,
+		printf((last_start%2==0) ? "%d: even\n" : "%d: odd\n", last_start),
+		start+=step
+
     );
     return 0;
 }
 
-// goes from start to end by step and prints whether the numbers are even or odd
+// goes from start(inclusive) to end(inclusive) by step and prints whether the numbers are even or odd
 // all only using a bodyless for
 //yes it does go backwards, do negative step
 //yes it will stop instead of running towards either one of the infinities if you try to set the step to be directed outwards the scope
