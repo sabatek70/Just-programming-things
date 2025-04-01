06cfe3f4cd30a0d8197c518c51714873728ea9b8
sabatek70 <137089474+sabatek70@users.noreply.github.com>
2025-04-02 00:59:32 +0200

    spaces fixed

--- OddEven.c
+++ OddEven.c
@@ -2,25 +2,25 @@
 
 int main(int argc, char* argv[])
 {
-    for
-    (
-        int input_start, input_limit, step, last_start,
-        print = printf("<start> <end> <step>\n"),
-        scan = scanf("%i%i%i", &input_start, &input_limit, &step),
-        start = input_start, limit = input_limit,
-        boundsBottom = (input_limit - input_start > 0 && step <= 0) ? (start = limit + 1 ) : 0,
-        boundsTop = (input_limit - input_start < 0 && step >= 0) ? (limit = start + 1 ) : 0;
-    
-        (input_start <= input_limit)
-        ? (start<=limit) //going up
-        : (limit<=start); //going down
+	for
+	(
+		int input_start, input_limit, step, last_start,
+		print = printf("<start> <end> <step>\n"),
+		scan = scanf("%i%i%i", &input_start, &input_limit, &step),
+		start = input_start, limit = input_limit,
+		boundsBottom = (input_limit - input_start > 0 && step <= 0) ? (start = limit + 1 ) : 0,
+		boundsTop = (input_limit - input_start < 0 && step >= 0) ? (limit = start + 1 ) : 0;
+
+		(input_start <= input_limit)
+		? (start<=limit) //going up
+		: (limit<=start); //going down
         
-        last_start = start,
+		last_start = start,
 		printf((last_start%2==0) ? "%d: even\n" : "%d: odd\n", last_start),
 		start+=step
 
-    );
-    return 0;
+	);
+	return 0;
 }
 
 // goes from start(inclusive) to end(inclusive) by step and prints whether the numbers are even or odd
